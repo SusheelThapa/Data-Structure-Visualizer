06fb4550e5bede31c12663bd39e2c9cd03281d54
Susheel Thapa <susheel.thapa@outlook.com>
2022-12-19 15:04:46 +0545

    Design the Stack visualizer on the right side
    - Add function to handle new button and bind with handler
    - Toast message display for other new button
    - Remove redundant component as Container
    - Update toastMessage to display toast of info

--- src/services/toastMessage.js
+++ src/services/toastMessage.js
@@ -1,19 +1,29 @@
 import { toast } from "react-toastify";
 
-export const toastMessage = (msg, type = "success") => {
-  if (type !== "success") {
+export const toastMessage = (msg, type) => {
+  if (type === "error") {
     toast.error(msg, {
       closeOnClick: true,
       draggable: true,
       progress: undefined,
     });
     return;
+  } else if (type === "success") {
+    toast.success(msg, {
+      hideProgressBar: false,
+      closeOnClick: true,
+      draggable: true,
+      progress: undefined,
+    });
+  } else if (type === "info") {
+    toast.info(msg, {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
   }
-
-  toast.success(msg, {
-    hideProgressBar: false,
-    closeOnClick: true,
-    draggable: true,
-    progress: undefined,
-  });
 };
